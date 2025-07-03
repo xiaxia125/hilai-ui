a8160311088f54aca45ae9395e13b03cbcc67c28
xiazhiwei6 <xiazhiwei6@hikvision.com>
2025-07-03 11:18:12 +0800

    chore: update package version to 1.5.2 and add h5player-js dependency

--- packages/VideoPlayer/src/index.vue
+++ packages/VideoPlayer/src/index.vue
@@ -9,8 +9,8 @@
 
   <script setup>
   import {ref, onMounted,onUnmounted} from 'vue'
+  import { ElMessage } from 'element-plus'
   import { loadScript } from '../../utils'
-
   const props = defineProps({
     url: {
       type: String,
@@ -27,10 +27,20 @@
   const player = ref(null)
 
   const id = ref(crypto.randomUUID())
-
+  async function loadH5PlayerScript() {
+    try {
+      // 使用动态导入
+      const h5playerModule = await import('h5player-js/h5player.min.js')
+      // 或者使用 URL 构造器
+      const scriptUrl = new URL('h5player-js/h5player.min.js', import.meta.url).href
+      await loadScript(scriptUrl)
+    } catch (error) {
+      console.error('加载失败:', error)
+    }
+  }
   onMounted(async () => {
     if (typeof window !== 'undefined') {
-      await loadScript('../assets/js/h5player.min.js') // 路径根据你的实际部署调整
+      await loadH5PlayerScript()
       if (window.JSPlugin) {
         initPlayer()
       }
@@ -55,39 +65,37 @@
    * 播放视频
    */
   function initPlayer() {
-    if (typeof window !== 'undefined' && window.JSPlugin) {
-      player.value = new window.JSPlugin({
-        // 需要英文字母开头 必填
-        szId: id.value,
-        oStyle: {
-          border: "transparent",
-          borderSelect: "transparent",
-        },
-      });
-      player.value.JS_SetWindowControlCallback({
-        windowEventSelect: function (index) {
-          //插件选中窗口回调
-          // console.log("windowSelect callback: ", index);
-        },
+    player.value = new window.JSPlugin({
+      // 需要英文字母开头 必填
+      szId: id.value,
+      oStyle: {
+        border: "transparent",
+        borderSelect: "transparent",
+      },
+    });
+    player.value.JS_SetWindowControlCallback({
+      windowEventSelect: function (index) {
+        //插件选中窗口回调
+        // console.log("windowSelect callback: ", index);
+      },
 
-        pluginErrorHandler: function (index, iErrorCode, oError) {
-          // 插件错误回调
-          // console.log("插件错误回调: ", index, iErrorCode, oError);
-        },
+      pluginErrorHandler: function (index, iErrorCode, oError) {
+        // 插件错误回调
+        // console.log("插件错误回调: ", index, iErrorCode, oError);
+      },
 
-        firstFrameDisplay: function (index, iWidth, iHeight) {
-          //首帧显示回调
-          // console.log(
-          //   `窗口${index}加载成功，分辨率为${iWidth}*${iHeight}`
-          // );
-        },
-        performanceLack: function () {
-          //性能不够回调
-        },
-      });
+      firstFrameDisplay: function (index, iWidth, iHeight) {
+        //首帧显示回调
+        // console.log(
+        //   `窗口${index}加载成功，分辨率为${iWidth}*${iHeight}`
+        // );
+      },
+      performanceLack: function () {
+        //性能不够回调
+      },
+    });
 
-      play(0);
-    }
+    play(0);
   }
 
   /**
